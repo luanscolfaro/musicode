6ea76155dcb73ca3eb62c7a8a7bdc7efedd9bc4b
Luan Scolfaro Amorim Carneiro <scolfaroluan@gmail.com>
2026-01-23 14:16:44 -0300

    Atualiza Footer: adiciona Instagram, ícones e negrito no nome

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,23 +1,30 @@
 import "./Footer.css";
+import { FaLinkedin, FaGithub, FaInstagram } from "react-icons/fa";
 
 function Footer() {
   return (
     <footer className="footer">
-      Desenvolvido por{" Luan Scolfaro "}
+      <span className="author">Desenvolvido por Luan Scolfaro</span>
       <a
         href="https://www.linkedin.com/in/luan-scolfaro-124498336/"
         target="_blank"
         rel="noopener noreferrer"
       >
-        Linked In
-      </a>{" "}
-      •{" "}
+        <FaLinkedin size={20} />
+      </a>
       <a
         href="https://github.com/luanscolfaro"
         target="_blank"
         rel="noopener noreferrer"
       >
-        GitHub
+        <FaGithub size={20} />
+      </a>
+      <a
+        href="https://www.instagram.com/seu_usuario_aqui/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaInstagram size={20} />
       </a>
     </footer>
   );
